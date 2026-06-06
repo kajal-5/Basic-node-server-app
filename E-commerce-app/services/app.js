a0871f75f2832961a937cbce0b2def28dddcbe2e
Kajal-Srivastav <kajal-5>
2026-06-06 17:07:57 +0530

    e-commerce-app

---
+++ E-commerce-app/services/app.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const app = express();
+// const userRouter= require("../routes/userRouter");
+const productRouter = require("../routes/productRouter")
+
+// app.use("/user",userRouter);
+app.use("/product",productRouter);
+
+app.listen(3000,()=>{
+    console.log("server running at 3000 port");
+})
