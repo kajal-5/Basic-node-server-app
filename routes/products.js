a852332b799598b99b611bc0fd607d07fd6f94d1
Kajal-Srivastav <kajal-5>
2026-06-06 15:17:58 +0530

    e-commerces controller products/id

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("./Controller/productController");
+const productController = require("../Controllers/productController");
 
 
 router.get("/",productController.getProducts);
